9c39ad838969298c15bd2962039208bc63301de7
Nitin Rangarajan <nitinr92@gmail.com>
2021-10-23 15:09:41 -0400

    Boilerplate

---
+++ Classes/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoilerPlate.Classes
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPassWord { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public List<string> PlacesToVisit { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
